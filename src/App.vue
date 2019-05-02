d2f5c9b59633803c1e8eb7ba2f55edca7b617e21
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2019-05-02 21:45:50 +0100

    Frst and last commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div id="view">
+    <Header v-if="['crawl'].indexOf($route.name)"/>
+    <router-view id="view"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from '@/components/Header'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
+<style >
+  body {
+    margin: 0
+  }
+</style>
+<style lang="css" scoped>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#view {
+  background-color: black;
+  height:100vh;
 }
 </style>
